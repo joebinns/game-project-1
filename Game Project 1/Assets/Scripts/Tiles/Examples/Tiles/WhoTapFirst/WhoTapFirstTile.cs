a45eca2ca88b511d5cdc1923c81c3f02b90cf1a6
VincenzoVK <vincenzo.catano@edu.futuregames.nu>
2022-10-12 17:43:01 +0200

    Almost working WhoTapFirst Tile
    Need to fix the number of times the Endevent is called and the UI

--- Game Project 1/Assets/Scripts/Tiles/Examples/Tiles/WhoTapFirst/WhoTapFirstTile.cs
+++ Game Project 1/Assets/Scripts/Tiles/Examples/Tiles/WhoTapFirst/WhoTapFirstTile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Managers.Points;
 using UnityEngine;
@@ -9,46 +10,55 @@ namespace Tiles.Examples
     public class WhoTapFirstTile : Tile
     {
         private bool _cooldownFinished = false;
-        private bool _eventFinished = false;
-        float t;
+        private float cooldownTimer;
+        [SerializeField] private float _countdownTime = 3f;
 
         public override void BeginEffect()
         {
             base.BeginEffect(); // Redirect inputs to this tile, play BeginEffectAudion and activate BeginEffectSprite.
             
+            Debug.Log("Who taps first begin event");
             
-            StartCoroutine(Cooldown(3f));
+            StartCoroutine(Cooldown(_countdownTime));
         }
         
         public override void EndEffect()
         {
+            
+            Debug.Log("Who taps first ends event");
+            
             // Call this method as the tile's last piece of logic!
             base.EndEffect(); // Redirect inputs back to player, play EndEffectAudio and deactivate effect sprite.
         }
 
         private IEnumerator Cooldown(float duration)
         {
-            t = duration;
-            while (t > 0)
+            cooldownTimer = duration;
+            while (cooldownTimer > 0)
             {
-                t -= Time.deltaTime;
+                cooldownTimer -= Time.deltaTime;
+                yield return null;
             }
             
             _cooldownFinished = true;
             yield return null;
+            EndEffect();
         }
 
         public override void HandleInput(int playerId)
         {
-            if (_cooldownFinished)
+            if (!_cooldownFinished)
             {
                 // Play HandleInputAudio
                 base.HandleInput(playerId);
 
                 PointsManager.GainPoints(playerId, 1000);
-                FindObjectOfType<UIHandler>().SetEffectText((3f-t).ToString("0.0#"));
+                
+                Debug.Log("Player" + playerId + " gained 1000 points!");
+                
+                FindObjectOfType<UIHandler>().SetEffectText((_countdownTime-cooldownTimer).ToString("0.0#"));
                 EndEffect();
             }
         }
-    }
+        }
 }
