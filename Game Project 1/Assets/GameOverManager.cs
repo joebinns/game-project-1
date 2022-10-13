592f1f5e677cc8ebfffa958fac1dd59f9fd116dd
Calle1122 <carl@lindstedt.net>
2022-10-13 11:04:19 +0200

    Main and GameOver
    Updated looks and functionality of menus.

---
+++ Game Project 1/Assets/GameOverManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI winnerNameTxt, loserNameTxt, winnerPointsTxt, loserPointsTxt;
+
+    private void Start()
+    {
+        //Set all txt's to the info provided by the gamemanager.
+    }
+}
