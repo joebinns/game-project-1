d40c22032db17a083484f7b72f22739cc7c3a219
Snov <snowconure@gmail.com>
2022-10-17 17:01:01 +0200

    speedlines

--- Game Project 1/Assets/Scripts/Players/HitEffects.cs
+++ Game Project 1/Assets/Scripts/Players/HitEffects.cs
@@ -11,23 +11,34 @@ public class HitEffects : MonoBehaviour
     [SerializeField] private List<Renderer> _renderers;
     [SerializeField] private AudioClip _ouchSound;
 
-    [SerializeField]  private const float FLASH_DURATION = 0.25f;
+    [SerializeField] private const float FLASH_DURATION = 0.25f;
 
     public void Play()
     {
-        /*AudioManager.PlaySound(_ouchSound);
+        AudioManager.PlaySound(_ouchSound);
         StartCoroutine(FlashMaterialCoroutine());
-        CameraManager.Main.Shake(20f, FLASH_DURATION);*/
+        CameraManager.Main.Shake(20f, FLASH_DURATION);
     }
-    
+
     private IEnumerator FlashMaterialCoroutine()
     {
-        /*foreach (Renderer renderer in _renderers)
-            renderer.materials[1].SetFloat("_Alpha", 1);
-*/
-        yield return new WaitForSeconds(FLASH_DURATION);
-/*
         foreach (Renderer renderer in _renderers)
-            renderer.materials[1].SetFloat("_Alpha", 0);*/
+            renderer.materials[1].SetFloat("_Alpha", 1);
+
+        yield return new WaitForSeconds(FLASH_DURATION / 3);
+
+        foreach (Renderer renderer in _renderers)
+            renderer.materials[1].SetFloat("_Alpha", 0);
+
+        yield return new WaitForSeconds(FLASH_DURATION / 3);
+
+        foreach (Renderer renderer in _renderers)
+            renderer.materials[1].SetFloat("_Alpha", 1);
+
+        yield return new WaitForSeconds(FLASH_DURATION / 3);
+
+
+        foreach (Renderer renderer in _renderers)
+            renderer.materials[1].SetFloat("_Alpha", 0);
     }
 }
