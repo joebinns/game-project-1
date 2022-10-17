7c65c5de53bf648eb5c533096eaadcd22acfc411
Snov <snowconure@gmail.com>
2022-10-17 14:10:29 +0200

    fix

--- Game Project 1/Assets/SpeedVFX.cs
+++ Game Project 1/Assets/SpeedVFX.cs
@@ -22,7 +22,7 @@ public class SpeedVFX : MonoBehaviour
         speedLines.SetFloat("SpawnRate", speed > 35 ? speed * 20:0);
         speedLines.SetFloat("Alpha", Mathf.Lerp(0,1, speed / 75));
         speedLines.SetFloat("SpeedMultiplier", speed * 0.15f);
-        CameraManager.Main.currentDefaultShake = Mathf.Lerp(.2f, 4, (speed - 35) / 75);
+        CameraManager.Main.currentDefaultShake = Mathf.Lerp(0f, 4, (speed - 35) / 75);
         Debug.Log("setting Speed");
     }
 }
