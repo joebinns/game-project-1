2a70ccd1946e7093eb1c9d940335a93f99ace56a
Joe Binns <joebinns.95@gmail.com>
2022-10-17 15:41:14 +0200

    Upgrade speed selector to have modes!!! awesome

--- Game Project 1/Assets/Scripts/RandomGenPack/SpeedSelector.cs
+++ Game Project 1/Assets/Scripts/RandomGenPack/SpeedSelector.cs
@@ -1,15 +1,25 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(RoadGenerator))]
 public class SpeedSelector : MonoBehaviour
-{
-    public float StartSpeed;
-    public float StopSpeed;
-    public float AbsoluteMaxSpeed = 100f;
-    [SerializeField] private float _rateOfIncrease;
+{ 
+    [SerializeProperty("SpeedMode")]
+    public SpeedMode _speedMode = SpeedMode.Low;
+    public SpeedMode SpeedMode
+    {
+        get => _speedMode;
+        set
+        {
+            StartCoroutine(TransitionSpeedMode(_speedMode, value));
+            _speedMode = value;
+        }
+    } 
+    [SerializeField] private float _transitionDuration;
     
+    [HideInInspector] public float PlayerCollisionMaxCalibratedSpeed = 100f;
     private RoadGenerator _roadGenerator;
     private float _t;
 
@@ -18,9 +28,25 @@ public class SpeedSelector : MonoBehaviour
         _roadGenerator = this.GetComponent<RoadGenerator>();
     }
 
-    void Update()
+    private IEnumerator TransitionSpeedMode(SpeedMode a, SpeedMode b)
     {
-        _t += Time.deltaTime;
-        _roadGenerator.roadSpeed = Mathf.Lerp(StartSpeed , StopSpeed, _t * _rateOfIncrease);
+        _t = 0f;
+        while (_t < _transitionDuration)
+        {
+            _t += Time.deltaTime;
+            _t = Mathf.Clamp(_t, 0f, _transitionDuration);
+            var speed = Mathf.Lerp((float)(int)a , (float)(int)b, _t * (1f / _transitionDuration));
+            _roadGenerator.roadSpeed = speed;
+            yield return null;
+        }
     }
+}
+
+public enum SpeedMode
+{
+    Stop = 0,
+    Low = 20,
+    Medium = 40,
+    High = 60,
+    GodSpeed = 200
 }
