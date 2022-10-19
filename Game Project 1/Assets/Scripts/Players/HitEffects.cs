631f6194bd65653a7c7e2d7d701fd8abcb42e309
Snov <snowconure@gmail.com>
2022-10-19 20:04:03 +0200

    gfh

--- Game Project 1/Assets/Scripts/Players/HitEffects.cs
+++ Game Project 1/Assets/Scripts/Players/HitEffects.cs
@@ -15,8 +15,12 @@ public class HitEffects : MonoBehaviour
     [SerializeField] private MeshRenderer _hemlet;
     [SerializeField] private AudioClip _ouchSound;
 
-    private Material[] defaultMat0;
-    private Material[] defaulthelmet;
+    public Material[] defaultMat0;
+    public Material[] defaultMatHurt;
+    public Material[] defaulthelmet;
+    public Material[] defaulthelmetHurt;
+
+    public Material hurtmaterial;
 
     [SerializeField] private const float FLASH_DURATION = 0.25f;
 
@@ -31,7 +35,23 @@ public class HitEffects : MonoBehaviour
         PlayOuchSound = FMODUnity.RuntimeManager.CreateInstance("event:/PlayerOuch1");
         PlayHoverCrashSound = FMODUnity.RuntimeManager.CreateInstance("event:/HoverCrash2");
 
-        defaultMat0 = _body
+        defaultMat0 = _body.materials;
+        defaulthelmet = _hemlet.materials;
+
+        
+        defaultMatHurt = defaultMat0;
+        defaulthelmetHurt = _hemlet.materials;
+
+       /* for (int i = 0; i < defaultMatHurt.Length; i++)
+        {
+            defaultMatHurt[i] = hurtmaterial;
+        }
+       */
+
+        for (int i = 0; i < defaulthelmetHurt.Length; i++)
+        {
+            defaulthelmetHurt[i] = hurtmaterial;
+        }
     }
     public void Play()
     {
@@ -42,17 +62,30 @@ public class HitEffects : MonoBehaviour
             //characterController.PlayHoverSoundLoop.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         }
 
-            StartCoroutine(FlashMaterialCoroutine());
-        
+        StartCoroutine(FlashMaterialCoroutine());
+
         //CameraManager.Main.Shake(20f, FLASH_DURATION);
     }
 
     private IEnumerator FlashMaterialCoroutine()
     {
-
+        _body.materials = defaultMatHurt;
+        _hemlet.materials = defaulthelmetHurt;
 
         yield return new WaitForSeconds(FLASH_DURATION / 3);
 
+        _body.materials = defaultMat0;
+        _hemlet.materials = defaulthelmet;
+
+        yield return new WaitForSeconds(FLASH_DURATION / 3);
+
+        _body.materials = defaultMatHurt;
+        _hemlet.materials = defaulthelmetHurt;
+
+        yield return new WaitForSeconds(FLASH_DURATION / 3);
+
+        _body.materials = defaultMat0;
+        _hemlet.materials = defaulthelmet;
         /* foreach(Material mat in _body.materials)
          _body.materials[_body.materials.Length-1].SetFloat("_Alpha", 1);
          _hemlet.materials[_hemlet.materials.Length-1].SetFloat("_Alpha", 1);
