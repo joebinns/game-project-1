c1755750cce5da964d0bb597772028cc7847697a
Fredrik Askebris <fredrik.askebris@edu.futuregames.nu>
2022-10-20 19:42:46 +0200

    fixed EventStart triggers

---
+++ Game Project 1/Assets/LaserSound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+
+public class LaserSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
